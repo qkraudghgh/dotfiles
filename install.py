94c050203b871bddb273aab38ae789adf77ef3c0
Haze Lee <hazelee@realignist.me>
2016-06-25 23:02:48 +0900

    Integrate IDEAVim.

--- install.py
+++ install.py
@@ -14,6 +14,7 @@ tasks = {
         # VIM
         '~/.vimrc' : 'vim/vimrc',
         '~/.vim' : 'vim',
+				'~/.ideavimrc' : 'vim/ideavimrc',
 
         # GIT
         '~/.gitconfig' : 'git/gitconfig',
