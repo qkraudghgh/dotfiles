98b90c78b15a78982d796bd73338ea92bfa1da57
Haze Lee <hazelee@realignist.me>
2016-06-25 22:45:31 +0900

    Update alias

--- zsh/zsh.d/alias.zsh
+++ zsh/zsh.d/alias.zsh
@@ -1,63 +1,11 @@
 # own aliasing 
 #
-# ssh ========================================= {{{
-alias bit='ssh lucidash@bit.sparcs.org'
-alias toy2='ssh user2@172.16.100.158'
-alias toy3='ssh user3@172.16.101.87'
-#alias sw='ssh user2@61.43.139.132'
-alias sw='ssh user2@61.43.139.142'
-alias sparcs='ssh lucidash@sparcs.org'
+# ssh =========================================
 
-alias tmdrb4='ssh lucidash@14.63.218.169'
-alias tmdrb2='ssh lucidash@14.63.218.219'
-alias tmdrb3='ssh lucidash@14.63.218.54'
+alias slog="ssh bot.realignist.me"
 
-alias vm='ssh 10.211.55.4'
-#alias tmdrb3='ssh lucidash@14.63.218.219 -p 24'
+# nginx
+alias n="sudo nginx"
 
-
-
-
-
-# VirtualEnv ========================================= {{{
-#
-alias venv='virtualenv'
-#
-# # }}}
-
-
-# Tmux ========================================= {{{
-#
-# # create a new session with name
-alias tmuxnew='tmux new -s'
-# # list sessions
-alias tmuxl='tmux list-sessions'
-# # tmuxa <session> : attach to <session> (force 256color and detach others)
-alias tmuxa='tmux -2 attach-session -d -t'
-#
-# # }}}
-
-
-
-# screen ========================================= {{{
-# 
-# # list sessions
-alias sls='screen -ls'
-# # create a new session with name 
-alias snew='screen -S'
-# # sdr <session> : attach to <session> and detach others
-alias sdr='screen -d -r'
-#
-# # }}}
-
-#tail -f to tailf
-alias tailf='tail -f'
-
-#others
-alias jj='python manage.py'
-alias l='ls -G'
-
-
-
-
-alias boj='python submit.py'
+alias su="sudo su"
+alias ls="ls -l"
